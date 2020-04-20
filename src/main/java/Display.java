b32f6f593350635981d8116a8bd7b72ad07b273e
smudgegame <atmargraff@gmail.com>
2020-04-20 14:08:08 -0400

    Display shows amount

--- src/main/java/Display.java
+++ src/main/java/Display.java
@@ -1,7 +1,16 @@
 public class Display {
 
-    public String show() {
-        return "INSERT COIN";
+    private static CoinManager coinManager;
+
+    public Display(CoinManager coinManager) {
+        this.coinManager = coinManager;
     }
+
+    public String show() {
+        String state = "INSERT COIN";
+        if (coinManager.currentSum() != 0) state = "$" + (double) coinManager.currentSum() / 100;
+        return state;
+    }
+
 
 }
--- src/test/java/DisplayTest.java
+++ src/test/java/DisplayTest.java
@@ -1,12 +1,30 @@
 import org.junit.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.junit.Assert.assertEquals;
 
 public class DisplayTest {
 
     @Test
     public void defaultDisplayState() {
-        Display display = new Display();
+        Map<Coin, Integer> valueMap = new HashMap<>();
+        Map<Coin, Integer> coinsHolding = new HashMap<>();
+        Map<Coin, Integer> coinReturn = new HashMap<>();
+        CoinManager coinManager = new CoinManager(valueMap, coinsHolding, coinReturn);
+        Display display = new Display(coinManager);
         assertEquals("INSERT COIN", display.show());
+    }
+
+    @Test
+    public void displaySum() {
+        Map<Coin, Integer> valueMap = new HashMap<>();
+        Map<Coin, Integer> coinsHolding = new HashMap<>();
+        Map<Coin, Integer> coinReturn = new HashMap<>();
+        CoinManager coinManager = new CoinManager(valueMap, coinsHolding, coinReturn);
+        coinManager.receiveCoin(Coin.QUARTER);
+        Display display = new Display(coinManager);
+        assertEquals("$0.25", display.show());
     }
 }
