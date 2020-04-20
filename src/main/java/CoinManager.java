fe7d0e132111751247e4d1583545a2cdc82cbf36
smudgegame <atmargraff@gmail.com>
2020-04-20 12:49:45 -0400

    Coins have value

--- src/main/java/CoinManager.java
+++ src/main/java/CoinManager.java
@@ -1,7 +1,22 @@
+import java.util.Map;
+
 public class CoinManager {
+
+    private static Map<Coin, Integer> valueMap;
+
+    public CoinManager(Map<Coin, Integer> valueMap) {
+        this.valueMap = valueMap;
+        valueMap.put(Coin.DIME, 10);
+        valueMap.put(Coin.NICKEL, 5);
+        valueMap.put(Coin.QUARTER, 25);
+    }
 
     public static boolean isValid(Coin coin) {
         if (coin.equals(Coin.PENNY)) return false;
         return true;
+    }
+
+    public static int getValue(Coin coin) {
+        return valueMap.get(coin);
     }
 }
--- src/test/java/CoinManagerTest.java
+++ src/test/java/CoinManagerTest.java
@@ -1,7 +1,9 @@
 import org.junit.Test;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.*;
 
 public class CoinManagerTest {
 
@@ -13,5 +15,14 @@ public class CoinManagerTest {
     @Test
     public void inNotValid() {
         assertFalse(CoinManager.isValid(Coin.PENNY));
+    }
+
+    @Test
+    public void coinValue() {
+        Map<Coin, Integer> valueMap = new HashMap<>();
+        CoinManager coinManager = new CoinManager(valueMap);
+        assertEquals(10, coinManager.getValue(Coin.DIME));
+        assertEquals(5, coinManager.getValue(Coin.NICKEL));
+        assertEquals(25, coinManager.getValue(Coin.QUARTER));
     }
 }
