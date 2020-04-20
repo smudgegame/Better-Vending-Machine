d273545952921c2d6cbe4f96b126af0ba16c1acd
smudgegame <atmargraff@gmail.com>
2020-04-20 09:52:02 -0400

    Penny is not a valid coin

--- src/main/java/CoinManager.java
+++ src/main/java/CoinManager.java
@@ -1,6 +1,7 @@
 public class CoinManager {
 
     public static boolean isValid(Coin coin) {
+        if (coin.equals(Coin.PENNY)) return false;
         return true;
     }
 }
--- src/test/java/CoinManagerTest.java
+++ src/test/java/CoinManagerTest.java
@@ -1,5 +1,6 @@
 import org.junit.Test;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class CoinManagerTest {
@@ -7,5 +8,10 @@ public class CoinManagerTest {
     @Test
     public void isValid() {
         assertTrue(CoinManager.isValid(Coin.DIME));
+    }
+
+    @Test
+    public void inNotValid() {
+        assertFalse(CoinManager.isValid(Coin.PENNY));
     }
 }
